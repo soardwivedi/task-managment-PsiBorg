ee8538d2e4c13542d34757b8c2ca5e281e56283c
Vivek <soarvivekdwivedi@gmail.com>
2024-12-23 23:47:50 +0530

    test

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+// const { errors } = require('celebrate');
+const authRoutes = require('./routes/users');
+require('dotenv').config();
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api', authRoutes);
+
+app.use(errors());
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
